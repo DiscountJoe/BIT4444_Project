a3616688c8b15c69ffb84d8fd2f3776985f891df
DiscountJoe <42777004+DiscountJoe@users.noreply.github.com>
2018-11-27 22:40:03 -0500

    Add files via upload

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
   define('DB_HOST', "localhost");   //localhost:8080
   define('DB_USER', "root");
   define('DB_PASS', "");
-  define('DB_NAME', "truckData");
+  define('DB_NAME', "trueTruckData");
  ?>
