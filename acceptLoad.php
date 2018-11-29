b45598250d9acf02a1efcbc372691a12cd0bd2f7
James Vasile <42777004+DiscountJoe@users.noreply.github.com>
2018-11-28 22:50:03 -0500

    Add files via upload

--- acceptLoad.php
+++ acceptLoad.php
@@ -1,1 +1,78 @@
-ACCEPT MY LOAD
+<!doctype html>
+<html>
+<head>
+  <title>Success</title>
+</head>
+<body>
+  <?php
+      session_start();
+
+    $state = "IT";//in transit
+    $CDL = $_SESSION["CDL"];//CDL
+    $dateFufilled = "N/A";//not applicable
+
+  /*  $destination = $_SESSION["destination"];
+    $dateListed = $_SESSION["dateListed"];
+    $weight = $_SESSION["weight"];
+    $origin = $_SESSION["origin"];
+    $rate=$_SESSION["rate"];
+    $clientID=$_SESSION['clientID'];//for integration with login page
+    $miles=$_SESSION['miles'];
+    $ratePerMile=($rate/$miles); */
+    $listingID=$_SESSION['listingID'];
+
+      echo "<a href='displayListings.php'>Back to Load Board</a>";
+
+
+    //$clientName=$_SESSION['clientName']; for integration with login page
+    //$clientName="Pedros inconspicuous shipping crates"; //placeholder until login integration
+
+    require_once("db.php");
+
+    $sql = "update listing set state = '$state', CDL = '$CDL' where listingID=$listingID";
+
+    /*"insert into listing
+            (       origin,     destination,   dateListed,     weight,    rate,   state,    CDL,    dateFufilled,    clientID,    miles,    ratePerMile,    clientName)
+            values ('$origin', '$destination', '$dateListed', '$weight', '$rate','$state', '$CDL', '$dateFufilled', '$clientID', '$miles', '$ratePerMile', '$clientName')";
+            */
+         $result=$mydb->query($sql);
+
+         if ($result==1) {
+
+           $sql2 = "select * from listing where listingID=$listingID";
+                $result=$mydb->query($sql2);
+                while($row = mysqli_fetch_array($result)){
+           echo "<p>You have accepted this Listing! And that's tea, sis!:</p></br>";
+
+           echo "<table>
+              <tr>
+                <th>  Client Name </th>
+                <th>  Origin  </th>
+                <th>  Destination </th>
+                <th>  date listed </th>
+                <th>  Weight  </th>
+                <th>  rate  </th>
+                <th>  Miles </th>
+                <th>  Rate per Mile </th>
+              </tr>
+              <tr>
+                <td>".$row['clientName']."</td>
+                <td>".$row['origin']."</td>
+                <td>".$row['destination']."</td>
+                <td>".$row['dateListed']."</td>
+                <td>".$row['weight']."</td>
+                <td>".$row['rate']."</td>
+                <td>".$row['miles']."</td>
+                <td>".$row['ratePerMile']."</td>
+              </tr>
+            </table>";
+                   }
+         }
+         else
+         {
+           echo "an error occured, please try again and ensure that the data is valid.";
+         }
+  ?>
+
+</body>
+</html>
