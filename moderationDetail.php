92978a91cdf8517a22ede4ccaa832d15aa94d6f6
James Vasile <42777004+DiscountJoe@users.noreply.github.com>
2018-11-28 21:51:06 -0500

    Add files via upload
    That's tea, sis!

--- moderationDetail.php
+++ moderationDetail.php
@@ -1,3 +1,13 @@
+<?php
+require_once("db.php");
+session_start();
+//$clientName=$_SESSION['clientName'];
+//$clientID=$_SESSION['clientID'];
+if (isset($_POST["submit"])) {
+    if(isset($_POST["listingID"])) $_SESSION['listingID']=$_POST["listingID"];
+    Header("Location:  disapprove.php");
+  }
+?>
 <!doctype html>
 <html>
 <head>
@@ -7,13 +17,11 @@
   <nav>
     <img src="reynholm.jpg" height="5%" width="5%">
     <a href="landing.php">Home</a> |
- <a href="timetable.php">Moderation</a> |
- <a href="record.php">Your Record</a> |
- <a href="advising.php">Advising</a> |
+ <a href="timetable.php">Moderation</a>
 </nav>
   <?php
     //resume the session variable on this page
-    session_start();
+  //  session_start();
     if (isset($_SESSION['listingID'])) $listingID=$_SESSION['listingID'];
 
     echo $listingID;
