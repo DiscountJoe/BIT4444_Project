8371f2a9fc2baa7a1f2bb9b8a88d208c2aacf383
James Vasile <42777004+DiscountJoe@users.noreply.github.com>
2018-11-28 22:15:08 -0500

    Add files via upload

--- logout.php
+++ logout.php
@@ -10,7 +10,9 @@
     echo "You have successfully logged out. Please <a href='who.html'>click here to login again</a>";
 
     session_start();
-    if (isset($_SESSION['listingID'])) $listingID=$_SESSION['listingID'];
+    if (isset($_SESSION['clientID'])) $_SESSION['clientID']="";
+      if (isset($_SESSION['adminID'])) $_SESSION['adminID']="";
+            if (isset($_SESSION['CDL'])) $_SESSION['CDL']="";
   ?>
 
 </body>
