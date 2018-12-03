a7f2afdba3edac057fb91674280b05657734190f
alxhggrty <42846618+alxhggrty@users.noreply.github.com>
2018-12-02 22:18:28 -0500

    Add files via upload

--- clientListingsPage.php
+++ clientListingsPage.php
@@ -1,19 +1,12 @@
 <?php
 session_start();
 
-require_once("db.php");
-if(isset($_SESSION['clientName'])) {$clientName=$_SESSION['clientName'];
-}else{Header("Location:  clientLogin.php");}
-if(isset($_SESSION['clientID'])) {$clientID=$_SESSION['clientID'];
-}else{Header("Location:  clientLogin.php");}
-
-if (isset($_POST["submit"])) {
-    if(isset($_POST["listingID"])) $_SESSION['listingID']=$_POST["listingID"];
-    Header("Location:  clientListingDetailView.php");
-  }
-
+if(isset($_SESSION['clientName'])) $clientName=$_SESSION['clientName'];
+if(isset($_SESSION['clientID'])) $clientID=$_SESSION['clientID'];
+require_once("db.php")
 ?>
-<html>
+
+<html lang="en" dir="ltr">
 <head>
   <title>Reynholm Industries</title>
 
@@ -21,22 +14,22 @@ if (isset($_POST["submit"])) {
    <link rel="stylesheet" href="stylesheet.css" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <meta charset="utf-8">
 </head>
-<body>
-      <img src="reynholm.jpg" height=5% width=5% />
-  <ul class="nav nav-tabs">
-  <li><a href="clientLanding.php">Home</a></li>
-  <li class="active"><a href="clientListingsPage.php">Your Listings</a></li>
-  <li><a href="clientCurrentLoads.php">Loads in Transit</a></li>
-  <li><a href="clientPastLoads.php">Past Loads</a></li>
-  <li><a href="createListing.php">Create Listing</a></li>
-</ul>
-
-  <p><div id="pieChart" style="margin: auto;
+	<body>
+		<img src="reynholm.jpg" height=5% width=5% />
+  	<ul class="nav nav-tabs">
+  		<li><a href="clientLanding.php">Home</a></li>
+  		<li class="active"><a href="clientListingsPage.php">Your Listings</a></li>
+  		<li><a href="clientCurrentLoads.php">Loads in Transit</a></li>
+  		<li><a href="clientPastLoads.php">Past Loads</a></li>
+  		<li><a href="createListing.php">Create Listing</a></li>
+		</ul>
+    <div id="pieChart" style="margin: auto;
   background-color:white;
   width: 50%;
   border: 3px solid black;
-  padding: 10px;"></div></p>
+  padding: 10px;"></div>
 
 <script src="//cdnjs.cloudflare.com/ajax/libs/d3/4.7.2/d3.min.js"></script>
 <script src="d3pie.min.js"></script>
@@ -146,105 +139,247 @@ var pie = new d3pie("pieChart", {
 	}
 });
 </script>
+		<table>
+		  <tr>
+		    <td>Origin:</td>
+		    <td><?php
+		    $sql="select distinct origin from listing where clientID='$clientID'";
+		    $result = $mydb->query($sql);
+		    echo "<select id='originDropdown' name='originDropdown'>";
+		    while($row=mysqli_fetch_array($result)){
+		      $Selection=$row["origin"];
+		      echo "<option value = '$Selection'>$Selection</option>";
+		    }
+		    echo "</select>";
+		    ?></td>
+		    <td>Maximum Rate/Mile:</td>
+		    <td><input type="number" id="maxRPM" name="maxRPM" value="" /></td>
+        <td>Minimum Rate/Mile:</td>
+       <td><input type="number" name="minRPM" id="minRPM" value="" /></td>
+
+		  </tr>
+		  <tr>
+		    <td>Destination:</td>
+		    <td><?php
+		    $sql="select distinct destination from listing where clientID='$clientID'";
+		    $result = $mydb->query($sql);
+		    echo "<select id='destinationDropdown'>";
+		    while($row=mysqli_fetch_array($result)){
+		      $Selection=$row["destination"];
+		      echo "<option value = '$Selection'>$Selection</option>";
+		    }
+		    echo "</select>";
+		    ?></td>
+        <td>Maximum Weight:</td>
+        <td><input type="number" id="maxWeight" name="maxWeight" value="" /></td>
+		    <td>Minimum Weight:</td>
+		    <td><input type="number" id="minWeight" name="minWeight" value="" /></td>
+		  </tr>
+		  <tr>
+        <td><button>Reset Search</button></td>
+		    <td><input type="hidden" name="state" id="state" value="" /></td>
+		    <td>Maximum Miles:</td>
+		    <td><input type="number" name="maxMiles" id="maxMiles" value="" /></td>
+		    <td>Minimum Miles:</td>
+		    <td><input type="number" name="minMiles" id="minMiles" value="" /></td>
+		  </tr>
+		</table>
+
+		<script src="jquery-3.1.1.min.js"></script>
+		<script>
+    $(function(){
+		$("#originDropdown").change(function(){
+			$.ajax({url:"displayProducts.php?originDropdown="+
+			$("#originDropdown").val()+"&maxRPM="+
+			$("#maxRPM").val()+"&maxWeight="+
+			$("#maxWeight").val()+
+			"&destinationDropdown="+
+			$("#destinationDropdown").val()+
+			"&minRPM="+$("#minRPM").val()+
+			"&minWeight="+$("#minWeight").val()+
+			"&dateListed="+$("#dateListed")+
+			"&minMiles="+$("#minMiles")+
+			"&maxMiles="+$("#maxMiles")+
+			"&state="+$("#state"),
+		    async:true,
+		    success:function(result){
+		      $("#contentArea").html(result);
+		    }
+		  })
+		})
+		})
+    $(function(){
+		$("#destinationDropdown").change(function(){
+			$.ajax({url:"displayProducts.php?originDropdown="+
+			$("#originDropdown").val()+"&maxRPM="+
+			$("#maxRPM").val()+"&maxWeight="+
+			$("#maxWeight").val()+
+			"&destinationDropdown="+
+			$("#destinationDropdown").val()+
+			"&minRPM="+$("#minRPM").val()+
+			"&minWeight="+$("#minWeight").val()+
+			"&dateListed="+$("#dateListed")+
+			"&minMiles="+$("#minMiles")+
+			"&maxMiles="+$("#maxMiles")+
+			"&state="+$("#state"),
+		    async:true,
+		    success:function(result){
+		      $("#contentArea").html(result);
+		    }
+		  })
+		})
+		})
+    $(function(){
+    $("#minRPM").change(function(){
+      $.ajax({url:"displayProducts.php?originDropdown="+
+      $("#originDropdown").val()+"&maxRPM="+
+      $("#maxRPM").val()+"&maxWeight="+
+      $("#maxWeight").val()+
+      "&destinationDropdown="+
+      $("#destinationDropdown").val()+
+      "&minRPM="+$("#minRPM").val()+
+      "&minWeight="+$("#minWeight").val()+
+      "&dateListed="+$("#dateListed")+
+      "&minMiles="+$("#minMiles")+
+      "&maxMiles="+$("#maxMiles")+
+      "&state="+$("#state"),
+        async:true,
+        success:function(result){
+          $("#contentArea").html(result);
+        }
+      })
+    })
+    })
+    $(function(){
+    		$("#minWeight").change(function(){
+    			$.ajax({url:"displayProducts.php?originDropdown="+
+    			$("#originDropdown").val()+"&maxRPM="+
+    			$("#maxRPM").val()+"&maxWeight="+
+    			$("#maxWeight").val()+
+    			"&destinationDropdown="+
+    			$("#destinationDropdown").val()+
+    			"&minRPM="+$("#minRPM").val()+
+    			"&minWeight="+$("#minWeight").val()+
+    			"&dateListed="+$("#dateListed")+
+    			"&minMiles="+$("#minMiles")+
+    			"&maxMiles="+$("#maxMiles")+
+    			"&state="+$("#state"),
+    		    async:true,
+    		    success:function(result){
+    		      $("#contentArea").html(result);
+    		    }
+    		  })
+    		})
+    		})
+        $(function(){
+        $("#minMiles").change(function(){
+          $.ajax({url:"displayProducts.php?originDropdown="+
+          $("#originDropdown").val()+"&maxRPM="+
+          $("#maxRPM").val()+"&maxWeight="+
+          $("#maxWeight").val()+
+          "&destinationDropdown="+
+          $("#destinationDropdown").val()+
+          "&minRPM="+$("#minRPM").val()+
+          "&minWeight="+$("#minWeight").val()+
+          "&dateListed="+$("#dateListed")+
+          "&minMiles="+$("#minMiles")+
+          "&maxMiles="+$("#maxMiles")+
+          "&state="+$("#state"),
+            async:true,
+            success:function(result){
+              $("#contentArea").html(result);
+            }
+          })
+        })
+        })
+        $(function(){
+        $("#maxWeight").change(function(){
+          $.ajax({url:"displayProducts.php?originDropdown="+
+          $("#originDropdown").val()+"&maxRPM="+
+          $("#maxRPM").val()+"&maxWeight="+
+          $("#maxWeight").val()+
+          "&destinationDropdown="+
+          $("#destinationDropdown").val()+
+          "&minRPM="+$("#minRPM").val()+
+          "&minWeight="+$("#minWeight").val()+
+          "&dateListed="+$("#dateListed")+
+          "&minMiles="+$("#minMiles")+
+          "&maxMiles="+$("#maxMiles")+
+          "&state="+$("#state"),
+            async:true,
+            success:function(result){
+              $("#contentArea").html(result);
+            }
+          })
+        })
+        })
+        $(function(){
+        $("#maxMiles").change(function(){
+          $.ajax({url:"displayProducts.php?originDropdown="+
+          $("#originDropdown").val()+"&maxRPM="+
+          $("#maxRPM").val()+"&maxWeight="+
+          $("#maxWeight").val()+
+          "&destinationDropdown="+
+          $("#destinationDropdown").val()+
+          "&minRPM="+$("#minRPM").val()+
+          "&minWeight="+$("#minWeight").val()+
+          "&dateListed="+$("#dateListed")+
+          "&minMiles="+$("#minMiles")+
+          "&maxMiles="+$("#maxMiles")+
+          "&state="+$("#state"),
+            async:true,
+            success:function(result){
+              $("#contentArea").html(result);
+            }
+          })
+        })
+        })
+        $(function(){
+        $("#maxRPM").change(function(){
+          $.ajax({url:"displayProducts.php?originDropdown="+
+          $("#originDropdown").val()+"&maxRPM="+
+          $("#maxRPM").val()+"&maxWeight="+
+          $("#maxWeight").val()+
+          "&destinationDropdown="+
+          $("#destinationDropdown").val()+
+          "&minRPM="+$("#minRPM").val()+
+          "&minWeight="+$("#minWeight").val()+
+          "&dateListed="+$("#dateListed")+
+          "&minMiles="+$("#minMiles")+
+          "&maxMiles="+$("#maxMiles")+
+          "&state="+$("#state"),
+            async:true,
+            success:function(result){
+              $("#contentArea").html(result);
+            }
+          })
+        })
+        })
+        $(function(){
+        $("#maxMiles").change(function(){
+          $.ajax({url:"displayProducts.php?originDropdown="+
+          $("#originDropdown").val()+"&maxRPM="+
+          $("#maxRPM").val()+"&maxWeight="+
+          $("#maxWeight").val()+
+          "&destinationDropdown="+
+          $("#destinationDropdown").val()+
+          "&minRPM="+$("#minRPM").val()+
+          "&minWeight="+$("#minWeight").val()+
+          "&dateListed="+$("#dateListed")+
+          "&minMiles="+$("#minMiles")+
+          "&maxMiles="+$("#maxMiles")+
+          "&state="+$("#state"),
+            async:true,
+            success:function(result){
+              $("#contentArea").html(result);
+            }
+          })
+        })
+        })
 
 
-<table>
-  <tr>
-    <td>Origin:</td>
-    <td><?php
-    $sql="select distinct origin from listing where clientID='$clientID'";
-    $result = $mydb->query($sql);
-    echo "<select id='originDropdown'>";
-    while($row=mysqli_fetch_array($result)){
-      $Selection=$row["origin"];
-      echo "<option value = '$Selection'>$Selection</option>";
-    }
-    echo "</select>";
-    ?></td>
-    <td>Maximum Rate/Mile:</td>
-    <td><input type="number" name="maxRPM" value="" /></td>
-    <td>Maximum Weight:</td>
-    <td><input type="number" name="maxWeight" value="" /></td>
-  </tr>
-  <tr>
-    <td>Destination:</td>
-    <td><?php
-    $sql="select distinct destination from listing where clientID='$clientID'";
-    $result = $mydb->query($sql);
-    echo "<select id='destinationDropdown'>";
-    while($row=mysqli_fetch_array($result)){
-      $Selection=$row["destination"];
-      echo "<option value = '$Selection'>$Selection</option>";
-    }
-    echo "</select>";
-    ?></td>
-    <td>Minimum Rate/Mile:</td>
-    <td><input type="number" name="minRPM" value="" /></td>
-    <td>Minimum Weight:</td>
-    <td><input type="number" name="minWeight" value="" /></td>
-  </tr>
-  <tr>
-    <td>Date Listed:</td>
-    <td><input type="date" name="dateListed" value="" /></td>
-    <td><input type="hidden" name="state" value="" /></td>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-</table>
+</script>
 
-<?php
-$sql = "select * from listing where clientID='$clientID' and clientName='$clientName'";
-$result = $mydb->query($sql);
-echo
-"<div><table>
-    <tr>
-      <th>  ListingID </th>
-      <th>  Client Name </th>
-      <th>  Origin  </th>
-      <th>  Destination </th>
-      <th>  date listed </th>
-      <th>  Weight  </th>
-      <th>  rate  </th>
-      <th>  Miles </th>
-      <th>  Rate per Mile </th>
-      <th>  Status </th>
-      <th>Detail View</th>
-    </tr>";
-
-  while($row = mysqli_fetch_array($result)) {
-    echo
-    "<tr>
-        <td>".$row['listingID']."</td>
-        <td>".$row['clientName']."</td>
-        <td>".$row['origin']."</td>
-        <td>".$row['destination']."</td>
-        <td>".$row['dateListed']."</td>
-        <td>".$row['weight']."</td>
-        <td>$".$row['rate']."</td>
-        <td>$".$row['miles']."</td>
-        <td>$".$row['ratePerMile']."</td>";
-
-        if ($row['state']=="NA"){echo "<td>Needs Approval</td>";}
-        elseif($row['state']=="L"){echo "<td>Listed</td>";}
-        elseif($row['state']=="IT"){echo "<td>In Transit</td>";}
-        elseif($row['state']=="F"){echo "<td>Fulfilled</td>";}
-        elseif($row['state']=="C"){echo "<td>Canceled</td>";}
-
-    echo"<td><form method='post'
-        action='".$_SERVER['PHP_SELF']."'>
-        <input type='hidden' name='listingID' value=".$row['listingID']." />
-        <input type='submit' name='submit' value='View More' />
-        </form></td>
-      </tr>
-
-      ";
-  }
-  echo "</div></table>";
-?>
-
-<div id='contentArea'></div>
-<p><a href="logout.php">Click here to log out</a></p>
-
+<div id="contentArea"></div>
 </body>
 </html>
---
+++ clientListingsPageBackend.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+require_once("db.php");
+if(isset($_SESSION['clientName'])) $clientName=$_SESSION['clientName'];
+if(isset($_SESSION['clientID'])) $clientID=$_SESSION['clientID'];
+
+echo
+"<div><table>
+    <tr>
+      <th>  ListingID &nbsp;</th>
+      <th>  Client Name &nbsp;</th>
+      <th>  Package Origin  &nbsp;</th>
+      <th>  Package Destination &nbsp;</th>
+      <th>  date listed &nbsp;&nbsp;&nbsp;</th>
+      <th>  Weight  (in tons)&nbsp;</th>
+      <th>  rate  &nbsp;&nbsp;&nbsp;</th>
+      <th>  Miles &nbsp;</th>
+      <th>  Rate per Mile &nbsp; </th>
+      <th>  Status &nbsp;</th>
+      <th>Detail View &nbsp;</th>
+    </tr>";
+
+$sql = "select * from listing where clientID='$clientID' and clientName='$clientName'";
+if(isset($_GET['originDropdown'])&&!($_GET['originDropdown']=="")&&!($_GET['originDropdown']=="[object Object]")) {
+  $sql=$sql." and origin='".$_GET['originDropdown']."'";}
+if((isset($_GET['minRPM'])&&!($_GET['minRPM']=="")&&!($_GET['minRPM']=="[object Object]"))&&
+(isset($_GET['maxRPM'])&&!($_GET['maxRPM']=="")&&!($_GET['maxRPM']=="[object Object]"))){
+  " and ratePerMile between '".$_GET['maxRPM']."' and '".$_GET['maxRPM']."'";}
+elseif(isset($_GET['minRPM'])&&!($_GET['minRPM']=="")&&!($_GET['minRPM']=="[object Object]")) {
+  $sql=$sql." and ratePerMile>='".$_GET['minRPM']."'";}
+elseif(isset($_GET['maxRPM'])&&!($_GET['maxRPM']=="")&&!($_GET['maxRPM']=="[object Object]")) {
+  $sql= $sql."and ratePerMile<='".$_GET['maxRPM']."'";}
+
+if((isset($_GET['maxWeight'])&&!($_GET['maxWeight']=="")&&!($_GET['maxWeight']=="[object Object]"))&&
+(isset($_GET['minWeight'])&&!($_GET['minWeight']=="")&&!($_GET['minWeight']=="[object Object]"))){
+" and weight between '".$_GET['maxWeight']."' and '".$_GET['minWeight']."'";}
+elseif(isset($_GET['maxWeight'])&&!($_GET['maxWeight']=="")&&!($_GET['maxWeight']=="[object Object]")) {
+  $sql=$sql." and weight<='".$_GET['maxWeight']."'";}
+elseif(isset($_GET['minWeight'])&&!($_GET['minWeight']=="")&&!($_GET['minWeight']=="[object Object]")) {
+    $sql=$sql." and weight>='".$_GET['minWeight']."'";}
+
+
+if(isset($_GET['destinationDropdown'])&&!($_GET['destinationDropdown']=="")&&!($_GET['destinationDropdown']=="[object Object]")) {
+  $sql=$sql." and destination='".$_GET['destinationDropdown']."'";}
+
+if((isset($_GET['minMiles'])&&!($_GET['minMiles']=="")&&!($_GET['minMiles']=="[object Object]"))&&
+(isset($_GET['maxMiles'])&&!($_GET['maxMiles']=="")&&!($_GET['maxMiles']=="[object Object]"))){
+  " and miles between '".$_GET['maxMiles']."' and '".$_GET['minMiles']."'";}
+if(isset($_GET['minMiles'])&&!($_GET['minMiles']=="")&&!($_GET['minMiles']=="[object Object]")) {
+  $sql=$sql." and miles>='".$_GET['minMiles']."'";}
+if(isset($_GET['maxMiles'])&&!($_GET['maxMiles']=="")&&!($_GET['maxMiles']=="[object Object]")) {
+    $sql=$sql." and miles<='".$_GET['maxMiles']."'";}
+
+$result = $mydb->query($sql);
+
+
+
+  while($row = mysqli_fetch_array($result)) {
+    echo
+    "<tr>
+        <td>".$row['listingID']."</td>
+        <td>".$row['clientName']."</td>
+        <td>".$row['origin']."</td>
+        <td>".$row['destination']."</td>
+        <td>".$row['dateListed']."</td>
+        <td>".$row['weight']."</td>
+        <td>$".$row['rate']."</td>
+        <td>$".$row['miles']."</td>
+        <td>$".$row['ratePerMile']."</td>";
+
+        if ($row['state']=="NA"){echo "<td>Needs Approval</td>";}
+        elseif($row['state']=="L"){echo "<td>Listed</td>";}
+        elseif($row['state']=="IT"){echo "<td>In Transit</td>";}
+        elseif($row['state']=="F"){echo "<td>Fulfilled</td>";}
+        elseif($row['state']=="C"){echo "<td>Canceled</td>";}
+
+    echo"<td><form method='post'
+        action='".$_SERVER['PHP_SELF']."'>
+        <input type='hidden' name='listingID' value=".$row['listingID']." />
+        <input type='submit' name='submit' value='View More' />
+        </form></td>
+      </tr>
+
+      ";
+  }
+  echo "</table></div>";
+?>
