7419abc0223e4f7d87d4f274d0a327bcba720c11
James Vasile <42777004+DiscountJoe@users.noreply.github.com>
2018-12-05 16:48:00 -0500

    Add files via upload

--- cancelListing.php
+++ cancelListing.php
@@ -1,8 +1,22 @@
 <!doctype html>
 <html>
 <head>
-  <title>Success</title>
+  <title>Reynholm Industries</title>
+
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   <link rel="stylesheet" href="stylesheet.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
+<body>
+      <img src="reynholm.jpg" height=5% width=5% />
+  <ul class="nav nav-tabs">
+  <li class="active"><a href="adminLanding.php">Home</a></li>
+  <li><a href="adminViewListings.php">View Listings</a></li>
+  <li><a href="moderationNew.php">Moderation</a></li>
+  <li><a href="adminViewCurrentListings.php">View Current Listings</a></li>
+    <li><a href="adminAreYouSure.php">Delete your Account</a></li>
+</ul>
 <body>
   <?php
     $state = "C";//cancelled
--- moderationDetail.php
+++ moderationDetail.php
@@ -16,13 +16,21 @@ if (isset($_POST["submit"])) {
 <html>
 <head>
   <title>Reynholm Industries</title>
+
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   <link rel="stylesheet" href="stylesheet.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-  <nav>
-    <img src="reynholm.jpg" height="5%" width="5%">
-    <a href="adminLanding.php">Home</a> |
- <a href="moderationNew.php">Moderation</a>
-</nav>
+      <img src="reynholm.jpg" height=5% width=5% />
+  <ul class="nav nav-tabs">
+  <li class="active"><a href="adminLanding.php">Home</a></li>
+  <li><a href="adminViewListings.php">View Listings</a></li>
+  <li><a href="moderationNew.php">Moderation</a></li>
+  <li><a href="adminViewCurrentListings.php">View Current Listings</a></li>
+    <li><a href="adminAreYouSure.php">Delete your Account</a></li>
+</ul>
   <?php
     //resume the session variable on this page
   //  session_start();
