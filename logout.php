3296706c8627ceae6f1dd37bac11370548002c94
James Vasile <42777004+DiscountJoe@users.noreply.github.com>
2018-12-05 18:19:35 -0500

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,13 +1,18 @@
 <!doctype html>
 <html>
 <head>
-  <title>Log out page</title>
+  <title>Reynholm Industries</title>
+
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   <link rel="stylesheet" href="stylesheet.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
   <img src="reynholm.jpg" height="5%" width="5%">
   <?php
-    echo "<p>Goodbye!</p>";
-    echo "You have successfully logged out. Please <a href='who.html'>click here to login again</a>";
+    echo "<div>Goodbye! ";
+    echo "You have successfully logged out. Please <a href='who.html'>click here to login again</a></div>";
 
     session_start();
     if (isset($_SESSION['clientID'])) $_SESSION['clientID']="";
